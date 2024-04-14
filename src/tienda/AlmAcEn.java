f9d978b430f675f1c40334287d42e457884aff97
DESKTOP-DH5U2UP\arturo <arturo_garcia_ms-tsapp1oa2324@cev.com>
2024-04-14 09:57:48 +0200

    Cambio de nombre en la clase Almacen para que sea más legible

--- src/tienda/AlmAcEn.java
+++ src/tienda/AlmAcEn.java
@@ -3,10 +3,10 @@ package tienda;
 
 import java.util.ArrayList;
 
-public class AlmAcEn {
+public class Almacen {
     public ArrayList<Producto> productos;
 
-    public AlmAcEn() {
+    public Almacen() {
         productos = new ArrayList<>();
     }
 }
