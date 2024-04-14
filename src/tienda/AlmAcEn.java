b47f70377d899d3ff5020d0206171a747b5fd704
DESKTOP-DH5U2UP\arturo <arturo_garcia_ms-tsapp1oa2324@cev.com>
2024-04-14 09:59:57 +0200

    Comentario añadido utilizando JavaDoc para un mayor feedback

--- src/tienda/AlmAcEn.java
+++ src/tienda/AlmAcEn.java
@@ -3,6 +3,12 @@ package tienda;
 
 import java.util.ArrayList;
 
+/**
+ * 
+ * @author arturo
+ * Clase almacen que tiene un arraylist para mostrar el listado de los productos a vender en la tienda
+ */
+
 public class AlmAcEn {
     public ArrayList<Producto> productos;
 
